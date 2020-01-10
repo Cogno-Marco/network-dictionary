3cd256a36959c27ab1415b1adb3875dc121bc0d4
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-10 14:54:56 +0100

    Improved Specifications + Added unit tests in SMSNetDictionaryTest - Removed example test class ~ Improved specifications for SMSNetDictionary

--- networklibrary/src/main/java/com/eis/communication/network/SMSNetDictionary.java
+++ networklibrary/src/main/java/com/eis/communication/network/SMSNetDictionary.java
@@ -11,6 +11,8 @@ public class SMSNetDictionary implements INetworkDictionary<String, String> {
      * Adds a resource to the network dictionary
      * @param key The key which defines the resource
      * @param resource The resource to add
+     * @throws IllegalArgumentException If the key is invalid.
+     * A key is said to be valid only if it's composed of a single word
      */
     public void addResource(String key, String resource){
 
@@ -19,6 +21,8 @@ public class SMSNetDictionary implements INetworkDictionary<String, String> {
     /**
      * Removes a resource from the dictionary
      * @param key The key which defines the resource
+     * @throws IllegalArgumentException If the key is invalid.
+     * A key is said to be valid only if it's composed of a single word
      */
     public void removeResource(String key){
 
@@ -29,6 +33,8 @@ public class SMSNetDictionary implements INetworkDictionary<String, String> {
      * @param key The key which defines the resource to get
      * @return Returns a resource corresponding to the key if present in the dictionary,
      * else returns null
+     * @throws IllegalArgumentException If the key is invalid.
+     * A key is said to be valid only if it's composed of a single word
      */
     public String getResource(String key){
         return null;
