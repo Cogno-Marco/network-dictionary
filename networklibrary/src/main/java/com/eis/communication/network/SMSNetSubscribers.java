9cfb26b7851ee9354bba1e69f7ee58c6ad013821
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-10 15:31:22 +0100

    Implemented SMSNetSubscribers class, every unit test was passed

--- networklibrary/src/main/java/com/eis/communication/network/SMSNetSubscribers.java
+++ networklibrary/src/main/java/com/eis/communication/network/SMSNetSubscribers.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  */
 public class SMSNetSubscribers implements INetSubscribers<SMSPeer>  {
 
+    private ArrayList<SMSPeer> subscribers = new ArrayList<>();
 
     /**
      * Adds a subscriber to this network
@@ -18,14 +19,15 @@ public class SMSNetSubscribers implements INetSubscribers<SMSPeer>  {
      * @throws IllegalArgumentException If subscriber is null
      */
     public void addSubscriber(SMSPeer subscriber){
-
+        if(subscriber == null) throw new IllegalArgumentException("Cannot add a null peer!");
+        subscribers.add(subscriber);
     }
 
     /**
      * @return Returns the list of all the current subscribers to the net
      */
     public ArrayList<SMSPeer> getSubscribers(){
-        return null;
+        return subscribers;
     }
 
     /**
@@ -34,6 +36,8 @@ public class SMSNetSubscribers implements INetSubscribers<SMSPeer>  {
      * @throws IllegalArgumentException If a non present subscriber is removed
      */
     public void removeSubcriber(SMSPeer subscriber){
-
+        if(!subscribers.contains(subscriber))
+            throw new IllegalArgumentException("The subscriber you're trying to remove is not present!");
+        subscribers.remove(subscriber);
     }
 }
