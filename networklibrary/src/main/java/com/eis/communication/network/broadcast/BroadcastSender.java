d70e42b3ba6fecd38b3bbfec995b047d6418a18b
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-11 14:43:43 +0100

    Implemented naive broadcast

--- networklibrary/src/main/java/com/eis/communication/network/broadcast/BroadcastSender.java
+++ networklibrary/src/main/java/com/eis/communication/network/broadcast/BroadcastSender.java
@@ -1,8 +1,25 @@
 package com.eis.communication.network.broadcast;
 
+import com.eis.communication.network.SMSNetSubscribers;
+import com.eis.smslibrary.SMSHandler;
+import com.eis.smslibrary.SMSMessage;
+import com.eis.smslibrary.SMSPeer;
+
 /**
  * This class allows to send a command in broadcast to the entire network
  */
 public class BroadcastSender {
+
+    /**
+     * Broadcasts a message to every subscriber on the net.
+     * @param message
+     * @param subscribers
+     */
+    public static void broadcastMessage(SMSMessage message, SMSNetSubscribers subscribers){
+        //Naive implementation of a broadcast: this node sends a message to each subscriber
+        for(SMSPeer peer : subscribers.getSubscribers()){
+            SMSHandler.getInstance().sendMessage(message);
+        }
+    }
 
 }
