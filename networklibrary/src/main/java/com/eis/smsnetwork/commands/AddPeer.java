8a7bcb15529d7db5bcf2352770385d3ef5cac965
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-12 10:21:35 +0100

    ~ Improved documentation for the 4 basic commands + Added Marco Cognolato as a commands author

--- networklibrary/src/main/java/com/eis/smsnetwork/commands/AddPeer.java
+++ networklibrary/src/main/java/com/eis/smsnetwork/commands/AddPeer.java
@@ -1,5 +1,7 @@
 package com.eis.smsnetwork.commands;
 
+import androidx.annotation.NonNull;
+
 import com.eis.communication.network.Command;
 import com.eis.smslibrary.SMSPeer;
 import com.eis.smsnetwork.SMSNetSubscriberList;
@@ -7,20 +9,25 @@ import com.eis.smsnetwork.SMSNetSubscriberList;
 /**
  * Command to add a peer to the Subscribers list
  *
- * @author Edoardo Raimondi
+ * @author Edoardo Raimondi, Marco Cognolato
  */
 public class AddPeer implements Command {
 
     private SMSPeer peer;
     private SMSNetSubscriberList netSubscribers;
 
-    public AddPeer(SMSPeer peer, SMSNetSubscriberList netSubscribers){
+    /**
+     * AddPeer command constructor, receives the data it needs to operate
+     * @param peer The Peer to add to the network
+     * @param netSubscribers The subscribers list to add the peer to
+     */
+    public AddPeer(@NonNull SMSPeer peer,@NonNull SMSNetSubscriberList netSubscribers){
         this.peer = peer;
         this.netSubscribers = netSubscribers;
     }
 
     /**
-     * Add the peer to subscribers
+     * Add the peer to the subscribers list and broadcasts it to the net
      */
     public void execute(){
         netSubscribers.addSubscriber(peer);
