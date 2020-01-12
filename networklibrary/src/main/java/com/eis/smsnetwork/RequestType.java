80eeb56dff6165d5f29103d8dfffec32969aa951
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-12 11:21:51 +0100

    Improved RequestType construction + Added method to get the RequestType from the the Request string name

--- networklibrary/src/main/java/com/eis/smsnetwork/RequestType.java
+++ networklibrary/src/main/java/com/eis/smsnetwork/RequestType.java
@@ -1,5 +1,10 @@
 package com.eis.smsnetwork;
 
+import android.os.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This enum contains the Request Types, which are the CODE placed at the beginning of every message
  * sent through the network. They identify the command contained in each message. Depending upon
@@ -28,5 +33,33 @@ public enum RequestType {
 
     public String asString() {
         return command;
+    }
+
+    private static final Map<String, RequestType> lookup = new HashMap<>();
+
+    /*
+     * Populate the lookup table on loading time
+     * Trick learned online
+     *
+     * @author Marco Cognolato
+     */
+    static
+    {
+        for(RequestType command : RequestType.values())
+        {
+            lookup.put(command.asString(), command);
+        }
+    }
+
+    /**
+     * Returns the RequestType given the string identifier
+     * @param command The String command of this
+     * @return Returns the corresponding RequestType if it exists, null otherwise
+     *
+     * @author Marco Cognolato
+     */
+    public static RequestType get(String command)
+    {
+        return lookup.get(command);
     }
 }
