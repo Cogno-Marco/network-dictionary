e51e9b661e3cbfc0a037497165adf91e127b21ec
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-12 14:13:02 +0100

    Improved specifications for SMSNetMessageBuilder

--- networklibrary/src/main/java/com/eis/smsnetwork/SMSNetMessageBuilder.java
+++ networklibrary/src/main/java/com/eis/smsnetwork/SMSNetMessageBuilder.java
@@ -3,8 +3,6 @@ package com.eis.smsnetwork;
 import com.eis.smslibrary.SMSMessage;
 import com.eis.smslibrary.SMSPeer;
 
-import java.util.Arrays;
-
 /**
  * This class is similar to the Director part of the
  * <a href="https://refactoring.guru/design-patterns/builder">Builder Design Pattern</a>,
@@ -18,30 +16,51 @@ public class SMSNetMessageBuilder extends MessageBuilder {
     private String commandMessage = null;
     private String messageText = null;
 
+    /**
+     * Constructor for this builder
+     */
     public SMSNetMessageBuilder() {
     }
 
+    /**
+     * Sets a peer as a Peer for this SMSMessage
+     *
+     * @param peer The peer to set
+     * @return Returns an instance of this builder to concatenate calls
+     */
     @Override
     public SMSNetMessageBuilder setPeer(SMSPeer peer) {
         super.setPeer(peer);
         return this;
     }
 
+    /**
+     * Sets the given request as the first parameter of the message
+     *
+     * @param command The RequestType to set
+     * @return Returns an instance of this builder to concatenate calls
+     */
     public SMSNetMessageBuilder setRequest(RequestType command) {
         commandMessage = command.asString();
         return this;
     }
 
+    /**
+     * Adds given arguments to the message, separated by a space
+     *
+     * @param message The parameters to add
+     * @return Returns an instance of this builder to concatenate calls
+     * @author Marco Cognolato
+     */
     @Override
     public SMSNetMessageBuilder addArguments(String... message) {
         //convert input to string
         String input = "";
-        if(message == null){
+        if (message == null) {
             input = "/";
-        }
-        else{
+        } else {
             StringBuilder builder = new StringBuilder(input);
-            for(String arg : message){
+            for (String arg : message) {
                 builder.append(arg);
                 builder.append(" ");
             }
@@ -49,9 +68,7 @@ public class SMSNetMessageBuilder extends MessageBuilder {
         }
 
         //add new input to old one
-
-
-        if(messageText == null)
+        if (messageText == null)
             messageText = input;
         else
             messageText += " " + input;
@@ -59,7 +76,7 @@ public class SMSNetMessageBuilder extends MessageBuilder {
     }
 
     /**
-     * @return the message ready to be sent
+     * @return The message ready to be sent
      */
     @Override
     public SMSMessage buildMessage() {
