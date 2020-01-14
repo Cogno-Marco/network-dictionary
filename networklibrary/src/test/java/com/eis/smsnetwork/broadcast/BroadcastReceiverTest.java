4e89fa867a9c7791e49fac2a18964434cad83c69
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2020-01-14 20:31:15 +0100

    ~ changed Strings associated to RequestTypes, now they're single GSM characters to save space in messages; ~ added tests for messages containing a valid request, but unwanted additional fields.

--- networklibrary/src/test/java/com/eis/smsnetwork/broadcast/BroadcastReceiverTest.java
+++ networklibrary/src/test/java/com/eis/smsnetwork/broadcast/BroadcastReceiverTest.java
@@ -2,6 +2,7 @@ package com.eis.smsnetwork.broadcast;
 
 import com.eis.smslibrary.SMSMessage;
 import com.eis.smslibrary.SMSPeer;
+import com.eis.smsnetwork.RequestType;
 import com.eis.smsnetwork.SMSJoinableNetManager;
 import com.eis.smsnetwork.SMSNetDictionary;
 import com.eis.smsnetwork.SMSNetSubscriberList;
@@ -12,9 +13,13 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
@@ -36,6 +41,44 @@ public class BroadcastReceiverTest {
         instance.onMessageReceived(garbage);
         PowerMockito.verifyStatic(never());
         SMSJoinableNetManager.getInstance();
+    }
+
+    @Test
+    public void onMessageReceived_InviteWithGarbage_IsIgnored() {
+        BroadcastReceiver instance = new BroadcastReceiver();
+        SMSPeer sender = new SMSPeer("+393492794133");
+        String garbageText = RequestType.Invite.asString() + ">";
+        SMSMessage garbageMessage = new SMSMessage(sender, garbageText);
+
+        SMSJoinableNetManager mockManager = mock(SMSJoinableNetManager.class);
+        when(mockManager.getNetSubscriberList()).thenReturn(new SMSNetSubscriberList());
+        when(mockManager.getNetDictionary()).thenReturn(new SMSNetDictionary());
+        PowerMockito.mockStatic(SMSJoinableNetManager.class);
+        when(SMSJoinableNetManager.getInstance()).thenReturn(mockManager);
+
+        instance.onMessageReceived(garbageMessage);
+        verify(mockManager, never()).checkInvitation(any());
+    }
+
+    @Test
+    public void onMessageReceived_RemovePeerWithGarbage_IsIgnored() {
+        BroadcastReceiver instance = new BroadcastReceiver();
+        SMSPeer sender = new SMSPeer("+393492794133");
+        String garbageText = RequestType.RemovePeer.asString() + ">";
+        SMSMessage garbageMessage = new SMSMessage(sender, garbageText);
+        Set<SMSPeer> subscribersSet = new HashSet<>();
+        subscribersSet.add(sender);
+
+        SMSNetSubscriberList mockSubscribers = mock(SMSNetSubscriberList.class);
+        when(mockSubscribers.getSubscribers()).thenReturn(subscribersSet);
+        SMSJoinableNetManager mockManager = mock(SMSJoinableNetManager.class);
+        when(mockManager.getNetSubscriberList()).thenReturn(mockSubscribers);
+        when(mockManager.getNetDictionary()).thenReturn(new SMSNetDictionary());
+        PowerMockito.mockStatic(SMSJoinableNetManager.class);
+        when(SMSJoinableNetManager.getInstance()).thenReturn(mockManager);
+
+        instance.onMessageReceived(garbageMessage);
+        verify(mockSubscribers, never()).removeSubscriber(any());
     }
 
     //TODO: write tests for messages with correct fields[0], and garbage in successive fields
