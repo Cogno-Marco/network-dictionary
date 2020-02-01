16680a54a6deb1ea41a2c39fce2be505db65bed5
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-02-01 12:21:50 +0100

    reset: removed some tests for the dictionary in preparation for pull-request

--- networklibrary/src/test/java/com/eis/smsnetwork/SMSNetDictionaryTest.java
+++ networklibrary/src/test/java/com/eis/smsnetwork/SMSNetDictionaryTest.java
@@ -14,7 +14,7 @@ public class SMSNetDictionaryTest {
 
     private final String KEY1 = "ResourceKey";
     private final String KEY2 = "OtherKey";
-    private final String INVALID_KEY = "This is not a valid key\\";
+    private final String INVALID_KEY = "This is not a valid key";
 
     private final String RESOURCE1 = "This is a valid resource";
     private final String RESOURCE2 = "This is another valid resource";
@@ -76,44 +76,5 @@ public class SMSNetDictionaryTest {
         netDictionary.addResource(KEY2, RESOURCE1);
         netDictionary.removeResource(KEY2);
         assertNull(netDictionary.getResource(KEY2));
-    }
-
-    @Test
-    public void addEscapes() {
-        String textToEscape = "hello,¤my¤name¤is¤Donald¤Duck¤\\¤";
-        String expectedText = "hello,\\¤my\\¤name\\¤is\\¤Donald\\¤Duck\\¤\\\\¤";
-        String escapedText = SMSNetDictionary.addEscapes(textToEscape);
-        assertEquals(expectedText, escapedText);
-    }
-
-    @Test
-    public void removeEscapes() {
-        String escapedText = "hello,\\¤my\\¤name\\¤is\\¤Donald\\¤Duck\\¤\\\\¤";
-        String expectedText = "hello,¤my¤name¤is¤Donald¤Duck¤\\¤";
-        String unescapedText = SMSNetDictionary.removeEscapes(escapedText);
-        assertEquals(expectedText, unescapedText);
-    }
-
-    @Test
-    public void getAllKeyValuePairsForSMS() {
-        netDictionary.addResource(KEY1, RESOURCE1.replaceAll(" ", "¤"));
-        netDictionary.addResource(KEY2, RESOURCE2.replaceAll(" ", "¤"));
-        String dictToString = netDictionary.getAllKeyResourcePairsForSMS();
-        String expectedString = "OtherKey¤This\\¤is\\¤another\\¤valid\\¤resource¤ResourceKey¤This\\¤is\\¤a\\¤valid\\¤resource";
-        assertEquals(expectedString, dictToString);
-    }
-
-    @Test
-    public void addResourceFromSMS() {
-        netDictionary.addResourceFromSMS("the\\¤cat\\¤is\\¤on", "the\\¤table");
-        assertEquals("the¤table", netDictionary.getResource("the¤cat¤is¤on"));
-    }
-
-    @Test
-    public void removeResourceFromSMS() {
-        netDictionary.addResourceFromSMS("the\\¤cat\\¤is\\¤on", "the\\¤table");
-        assertEquals("the¤table", netDictionary.getResource("the¤cat¤is¤on"));
-        netDictionary.removeResource("the¤cat¤is¤on");
-        assertNull(netDictionary.getResource("the¤cat¤is¤on"));
     }
 }
