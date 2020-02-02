23e675a477a128008c15fe9b5b441892a75ed266
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-02-02 12:39:49 +0100

    fix: made inviter private, style: formatted code

--- networklibrary/src/main/java/com/eis/smsnetwork/SMSInvitation.java
+++ networklibrary/src/main/java/com/eis/smsnetwork/SMSInvitation.java
@@ -1,6 +1,5 @@
 package com.eis.smsnetwork;
 
-import com.eis.communication.Peer;
 import com.eis.communication.network.Invitation;
 import com.eis.smslibrary.SMSPeer;
 
@@ -8,16 +7,17 @@ import com.eis.smslibrary.SMSPeer;
  * Represents a received invitation to join a network.
  */
 public class SMSInvitation implements Invitation<SMSPeer> {
-    SMSPeer inviter;
+    private SMSPeer inviter;
+
     public SMSInvitation(SMSPeer inviter) {
         this.inviter = inviter;
     }
 
 
-    @Override
     /**
      * Returns the {@link SMSPeer} who sent the Invitation to join a new network
      */
+    @Override
     public SMSPeer getInviterPeer() {
         return inviter;
     }
